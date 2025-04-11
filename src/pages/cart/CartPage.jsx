da496f1b98c6624e81648a991cd54a7874cb15d6
ela.md <ela.ahmadi06@gmail.com>
2025-04-11 18:55:26 -0400

    initial commit (folder structure)

---
+++ src/pages/cart/CartPage.jsx
@@ -0,0 +1,13 @@
+
+import './CartPage.css'
+
+function CartPage () {
+
+    return (
+        <>
+        <h1>CartPage</h1>
+        </>
+    )
+}
+
+export default CartPage
---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,13 @@
+
+import './HomePage.css'
+
+function HomePage () {
+
+    return (
+        <>
+        <h1>HomePage</h1>
+        </>
+    )
+}
+
+export default HomePage
