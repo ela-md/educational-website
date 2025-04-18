fe540a7012c617cbdc2069e24b6965caf1c1ffb3
ela.md <ela.ahmadi06@gmail.com>
2025-04-18 12:53:37 -0400

    setup darkmode (body)

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,16 @@ import CartPage from './pages/cart/CartPage'
 import AboutePage from './pages/about/AboutPage'
 import ContactPage from './pages/contact/ContactPage'
 import Navbar from './components/navbar/Navbar'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 
 function App() {
 
+const {theme} = useSelector(store => store.global)
+
+  useEffect(()=>{
+  document.body.classList.toggle('dark')
+  }, [theme])
 
   return (
   <>
