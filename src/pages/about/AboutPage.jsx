8206a7273589acc9266ae0f724e7461f97769bbe
ela.md <ela.ahmadi06@gmail.com>
2025-04-25 19:02:48 -0400

    update pages

--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -1,12 +1,22 @@
 
+import { useSelector } from 'react-redux'
 import './AboutPage.css'
 
 function AboutPage () {
-
+const {theme} = useSelector((store) => store.global)
     return (
-        <>
-        <h1>About</h1>
-        </>
+        <div className="about">
+        <div className="container">
+            <div className={`aboutInfo ${theme}`}>
+<h2>about us:</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis cupiditate quia corrupti adipisci autem soluta eveniet assumenda, eligendi nostrum dolorum sed error ex harum culpa veniam aliquid recusandae, nesciunt magnam.
+Fugit pariatur repellendus ipsam quae provident expedita veniam porro reprehenderit optio a animi, minus voluptas aliquid molestiae laboriosam, sunt distinctio aperiam placeat maiores quam cum excepturi deserunt eius. Laborum, nesciunt?
+Beatae nemo vero voluptatum eaque quis, id veniam? Excepturi, quibusdam? Debitis saepe repellendus adipisci aut sed, similique blanditiis doloribus provident fugiat, sequi modi vitae dolore dolorum odit ab laborum optio.
+Voluptate harum accusantium nihil, sit ipsum ea in error ratione ullam, quo minus alias non ex similique neque consequatur animi atque? Nemo dolorem tempora ad ut at, nesciunt voluptatem voluptatum!
+</p>
+            </div>
+        </div>
+       </div>
     )
 }
 
--- src/pages/contact/ContactPage.jsx
+++ src/pages/contact/ContactPage.jsx
@@ -1,12 +1,19 @@
 
 import './ContactPage.css'
+import { useSelector } from 'react-redux'
 
 function ContactPage () {
+    const { theme } = useSelector((store) => store.global)
 
     return (
-        <>
-        <h1>Contact</h1>
-        </>
+       <div className="contact">
+        <div className="container">
+            <div className={`contactInfo ${theme}`}>
+              <h2>more inquiries :</h2>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum cupiditate nihil tempora dolorem.</p>
+            </div>
+        </div>
+       </div>
     )
 }
 
